a39b6975372936822ce366682904aa1032c25fb0
Mide001 <olamideisreal5@gmail.com>
2023-12-11 12:03:19 +0100

    Implement time using block.timestamp

--- contracts/IDOLaunchpad.sol
+++ contracts/IDOLaunchpad.sol
@@ -8,19 +8,23 @@ import "./SafeMath.sol";
 contract IDOLaunchpad {
     using SafeMath for uint256;
 
-
     address public owner;
     uint256 public tokenPrice;
     uint256 public minInvestment;
     uint256 public maxInvestment;
-    uint256 public saleDuration;
+    uint256 public saleDurationInSeconds;
     uint256 public totalTokensSold;
     bool public isSaleActive;
     IERC20 public memeCoinToken;
+
+    uint256 public saleStartTime;
+    uint256 public saleEndTime;
+
     mapping (address => uint256) public investments;
     mapping (address => uint256) public tokensPurchased;
 
-
+    event SaleStarted(uint256 startTime, uint256 endTime);
+    event SaleStopped(uint256 endTime);
     event TokensPurchased(address indexed buyer, uint256 amount, uint256 cost);
 
     modifier onlyOwner() {
@@ -28,43 +32,45 @@ contract IDOLaunchpad {
         _;
     }
 
-    constructor(address _tokenAddress, uint256 _price, uint256 _minInvestment, uint256 _maxInvestment, uint256 _duration) {
+    constructor(address _tokenAddress, uint256 _price, uint256 _minInvestment, uint256 _maxInvestment, uint256 _durationInDays) {
         owner = msg.sender;
         memeCoinToken = IERC20(_tokenAddress);
         tokenPrice = _price;
         minInvestment = _minInvestment;
         maxInvestment = _maxInvestment;
-        saleDuration = _duration;
+        saleDurationInSeconds = _durationInDays * 1 days;
         isSaleActive = false;
     }
 
     function startSale() external onlyOwner {
-        require(isSaleActive, "Sale is already active");
+        require(!isSaleActive, "Sale is already active");
+        saleStartTime = block.timestamp;
+        saleEndTime = saleStartTime + saleDurationInSeconds;
         isSaleActive = true;
+        emit SaleStarted(saleStartTime, saleEndTime);
     }
 
     function stopSale() external onlyOwner {
         require(isSaleActive, "Sale is not active");
         isSaleActive = false;
+        emit SaleStopped(block.timestamp);
     }
 
     function buyTokens(uint256 _amount) external payable {
         require(isSaleActive, "Sale is not active");
+        require(block.timestamp <= saleEndTime, "Sale has ended");
         require(msg.value >= minInvestment, "Amount sent is below the minimum investment");
         require(msg.value <= maxInvestment, "Amount sent exceeds the maximum investment");
         require(totalTokensSold.add(_amount) <= memeCoinToken.balanceOf(address(this)), "Insufficient tokens available for sale");
 
-
         uint256 cost = _amount.mul(tokenPrice);
         require(msg.value >= cost, "Insufficient funds sent");
-
 
         memeCoinToken.transfer(msg.sender, _amount);
         investments[msg.sender] = investments[msg.sender].add(msg.value);
         tokensPurchased[msg.sender] = tokensPurchased[msg.sender].add(_amount);
         totalTokensSold = totalTokensSold.add(_amount);
 
-        // Emit The Token Purchased Event
         emit TokensPurchased(msg.sender, _amount, cost);
     }
 
@@ -89,7 +95,6 @@ contract IDOLaunchpad {
         memeCoinToken.transfer(msg.sender, tokensToClaim);
     }
 
-
     function participateInGame(uint256 _amount) external {
         require(isSaleActive, "Sale is not active");
         require(_amount > 0, "Invalid amount");
@@ -99,14 +104,12 @@ contract IDOLaunchpad {
         uint256 tokensToReceive = _amount.mul(tokenPrice).div(1e18);
         uint256 totalInvestment = _amount.sub(gasFee);
 
-
         memeCoinToken.transferFrom(owner, msg.sender, tokensToReceive);
 
         investments[msg.sender] = investments[msg.sender].add(totalInvestment);
         tokensPurchased[msg.sender] = tokensPurchased[msg.sender].add(tokensToReceive);
         totalTokensSold = totalTokensSold.add(tokensToReceive);
 
-        // Emit The Token Purchased Event
         emit TokensPurchased(msg.sender, tokensToReceive, totalInvestment);
-}
+    }
 }
